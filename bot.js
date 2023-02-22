34152bd3b4605561f515b9da20b8ac927a7fe3ab
Isabel Matas Rabadan <isabel.matas.rabadan@outlook.es>
2023-02-22 22:20:46 +0100

    Changed regex

--- bot.js
+++ bot.js
@@ -17,19 +17,20 @@ bot.onText(/^\/start/, function (msg) {
     bot.sendMessage(chatId, `Hola ${nameUser} !`);
 }); */
 
-bot.onText(/^Apostado/, function (msg) {
+bot.onText(/^[0-9] \w*/, function (msg) {
     var chatId = msg.chat.id;
     const opts = {
         reply_to_message_id: msg.message_id,
         one_time_keyboard: true
       };
-    const params = msg.text.split(' ');
-    if(!params || params.length != 3) {
+    bot.sendMessage(chatId, `Recibido`, opts);
+    /*const params = msg.text.split(' ');
+     if(!params || params.length != 3) {
         bot.sendMessage(chatId, `Formato incorrecto`, opts);
         bot.sendMessage(chatId, `El formato debe ser:\n Apostado numero_juego nombre_jugador`);
     } else {
         bot.sendMessage(chatId, `Recibido`, opts);
-    }    
+    }  */   
 });
 /* 
 bot.on('callback_query', (query) => {
